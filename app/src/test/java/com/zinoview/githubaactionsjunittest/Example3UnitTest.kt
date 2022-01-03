f95fa55b13df8d74bd8d0eb473c70808485cfaaf
kostyagig <k.gig@list.ru>
2022-01-03 21:42:36 +0300

    update yml

---
+++ app/src/test/java/com/zinoview/githubaactionsjunittest/Example3UnitTest.kt
@@ -0,0 +1,17 @@
+package com.zinoview.githubaactionsjunittest
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class Example3UnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(7, 5 + 2)
+    }
+}
